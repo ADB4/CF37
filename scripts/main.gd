a90b7d211d053d8b263f66ade67b8e2dd91bf9d9
andydbui <buiand@fastmail.com>
2026-07-18 14:13:48 -0500

    debugging mouse escape

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,5 +1,29 @@
 extends Node3D
 
+@onready var _player: PlayerController = $PlayerController
+
+func _ready() -> void:
+	_player.charge_started.connect(_on_charge_started)
+	_player.charge_updated.connect(_on_charge_updated)
+	_player.charge_released.connect(_on_charge_released)
+	_player.pie_thrown.connect(_on_pie_thrown)
+	
+func _on_charge_started() -> void:
+	print(">>> charge_started")
+	
+func _on_charge_updated(ratio: float) -> void:
+	if absf(ratio - roundf(ratio * 4.0) / 4.0) < 0.02:
+		print("   charge_updated: %.0f%%" % (ratio * 100.0))
+
+func _on_charge_updated_raw(_ratio: float) -> void:
+	pass
+	
+func _on_charge_released() -> void:
+	print("<<< charge_released")
+	
+func _on_pie_thrown(pie: PieProjectile) -> void:
+	print("=== pie_thrown (speed %.1f)" % pie.linear_velocity.length())
+
 func _process(_delta: float) -> void:
 	var axis := Input.get_axis("move_left", "move_right")
 	if absf(axis) > 0.01:
--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -1,12 +1,16 @@
 class_name PlayerController
 extends Node3D
 
+signal charge_started
+signal charge_updated(ratio: float)
+signal charge_released
+signal pie_thrown(pie: PieProjectile)
+
 var active := true:
 	set(value):
 		active = value
-		if not value:
-			# future: cancel in-flight charge
-			pass
+		if not value and _charging:
+			_cancel_charge()
 
 @export_group("Look")
 @export var mouse_sensitivity := 0.0022
@@ -21,10 +25,17 @@ var _pitch := 0.0
 @export var min_x := -3.2
 @export var max_x := 3.2
 
-@export_group("Throw (temp)")
+@export_group("Throw")
 @export var pie_scene: PackedScene
 @export var projectile_parent: Node3D
-@export var test_throw_speed := 18.0
+@export var min_throw_speed := 8.0
+@export var max_throw_speed := 22.0
+@export var full_charge_time := 0.6
+@export var throw_cooldown := 0.25
+
+var _charging := false
+var _charge_time := 0.0
+var _cooldown_remaining := 0.0
 
 @onready var _camera: Camera3D = $Camera3D
 @onready var _spawn_point: Marker3D = $Camera3D/SpawnPoint
@@ -36,6 +47,7 @@ func _process(delta: float) -> void:
 	if not active:
 		return
 	_handle_movement(delta)
+	_handle_throw(delta)
 		
 func _handle_movement(delta: float) -> void:
 	var axis := Input.get_axis("move_left", "move_right")
@@ -55,14 +67,63 @@ func _unhandled_input(event: InputEvent) -> void:
 		)
 		rotation.y = _yaw
 		_camera.rotation.x = _pitch
-
-	if active and event.is_action_pressed("throw") and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
-		_test_throw()
-
-func _test_throw() -> void:
-	if pie_scene == null or projectile_parent == null:
+	
+	if event is InputEventMouseButton and event.pressed and Input.mouse_mode != Input.MOUSE_MODE_CAPTURED:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+		get_viewport().set_input_as_handled()
 		return
+		
+	if not active:
+		return
+		
+	if event.is_action_pressed("throw") and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		if _cooldown_remaining <= 0.0 and not _charging:
+			_charging = true
+			_charge_time = 0.0
+			charge_started.emit()
+			charge_updated.emit(0.0)
+			
+	if event.is_action_released("throw") and _charging:
+		_throw()
+
+# throwing logic
+func _handle_throw(delta: float) -> void:
+	if _cooldown_remaining > 0.0:
+		_cooldown_remaining -= delta
+		
+	if _charging:
+		_charge_time = minf(_charge_time + delta, full_charge_time)
+		var ratio := _charge_time / full_charge_time
+		charge_updated.emit(ratio)
+		
+		if Input.mouse_mode != Input.MOUSE_MODE_CAPTURED:
+			_cancel_charge()
+		
+func _throw() -> void:
+	_charging = false
+	var ratio := _charge_time / full_charge_time
+	charge_released.emit()
+	
+	if pie_scene == null or projectile_parent == null: 
+		return
+		
 	var pie: PieProjectile = pie_scene.instantiate()
 	projectile_parent.add_child(pie)
 	pie.global_transform = _spawn_point.global_transform
-	pie.launch(-_camera.global_transform.basis.z, test_throw_speed)
+	
+	var speed := lerpf(min_throw_speed, max_throw_speed, ratio)
+	pie.launch(-_camera.global_transform.basis.z, speed)
+	
+	pie_thrown.emit(pie)
+	_cooldown_remaining = throw_cooldown
+	
+	
+func _cancel_charge() -> void:
+	_charging = false
+	_charge_time = 0.0
+	charge_released.emit()
+	
+func reset_for_round() -> void:
+	if _charging:
+		_cancel_charge()
+	_cooldown_remaining = 0.3
